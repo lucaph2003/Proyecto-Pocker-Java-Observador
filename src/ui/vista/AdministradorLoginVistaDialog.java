c16b22795dc235ed08a1fd54ea85d9c3f1df035d
Luca Podesta <lucapodesta47@gmail.com>
2024-11-11 21:30:39 -0300

    Aplico cambios de package

---
+++ src/ui/vista/AdministradorLoginVistaDialog.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui.vista;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author lpodesta
+ */
+public class AdministradorLoginVistaDialog extends LoginVistaDialog{
+    
+    public AdministradorLoginVistaDialog() {
+        this.setTitle("Login Administrador");
+    }
+
+    @Override
+    protected void ejecutarSiguienteCU(String usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected void login(String nombreDeUsuario, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
